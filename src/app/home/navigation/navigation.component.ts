8e42b7e3b8aa9779504b34139effc41687ec3716
Sami Younas <samiys005@gmail.com>
2023-02-15 01:25:02 +0500

    Navigation search working

--- src/app/home/navigation/navigation.component.ts
+++ src/app/home/navigation/navigation.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Observable, firstValueFrom } from 'rxjs';
+import { NavigationService } from './navigation.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
+  @ViewChild('toggleButton') toggleButton: ElementRef;
+  @ViewChild('menu') menu: ElementRef;
+  searchTerm: string;
+  artistsName: string[] = [];
+  results$: Observable<any[]>;
+  searchText = '';
+  isMenuOpen = false;
 
-  constructor() { }
+  constructor(
+    private navigationSerivce: NavigationService,
+    private renderer: Renderer2,
+    private _eref: ElementRef
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.renderer.listen('document', 'click',(e:Event)=> {
+      this.isMenuOpen=false;
+      // if(e.target !== this.toggleButton.nativeElement && e.target!==this.menu.nativeElement){
+      // }
+    });
+  }
+
+  toggleMenu() {
+    console.log('menu');
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  async openMenu(e) {
+    if(this.searchTerm.length > 0) {
+      this.isMenuOpen = true;
+      this.artistsName = [];
+      this.navigationSerivce.searchArtist(this.searchTerm).subscribe(resp => {
+        resp.results.artistmatches.artist.map(artist => {
+          this.artistsName.push(artist.name);
+        });
+      });
+      console.log('artistsName: ', this.artistsName);
+      return;
+    }
+    this.isMenuOpen = false;
   }
 
 }
--- src/app/home/navigation/navigation.service.ts
+++ src/app/home/navigation/navigation.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, concatMap, forkJoin, last, map, of, shareReplay, switchMap, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  searchArtist(artist: string): Observable<any> {
+    return this.http.get<any>(environment.apiUrl + `artist.search&artist=${artist}&api_key=${environment.api_key}&format=json`)
+        .pipe(map( (response: any) => {
+          return response;
+            }), last(), catchError((error: HttpErrorResponse) => throwError(error.error)),
+            shareReplay(1)
+        );
+  }
+
 }
